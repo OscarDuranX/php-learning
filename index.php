ee2a9255ea6acceffbcafb91c8c84a847a5e434e
Oscar Duran <xoscarduranx@gmail.com>
2016-09-15 21:56:28 +0200

    php & html

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
-    <h1>
-<!--        Sanejar codi-->
-        <?= 'Hello ' . htmlspecialchars($_GET['name']);?>
-    </h1>
-
-</body>
-</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oscar
+ * Date: 15/09/16
+ * Time: 21:56
+ */
