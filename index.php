e94cf85091f965fe61f85c2d9f8e61b44ec7d73c
Oscar Duran <xoscarduranx@gmail.com>
2016-09-16 15:53:43 +0200

    arrays i vectors

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-$greeting = "Hola %_GET['name']!";
+//$greeting = "Hola %_GET['name']!";
 
-require 'index.template.php';
+//require 'index.template.php';
+
+//Estrcutres de dades:
+// Arrays PHP totes les estrcuyutres : Maps, Piles, Vectors, Listes,  etc
+
+//Declarar arrays
+// 1- $names = array();
+$names = ['Santiago', 'Compostela', 'Sonotone'];
