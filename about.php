f8b9608d9bb5006a069693333be79f78f96dcc51
Oscar Duran <xoscarduranx@gmail.com>
2016-09-22 21:22:54 +0200

    menu about contact

--- about.php
+++ about.php
@@ -1,7 +1,15 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: oscar
- * Date: 22/09/16
- * Time: 21:18
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>About</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur esse incidunt laudantium mollitia nobis omnis placeat quam repellat, tenetur veritatis? Accusamus aliquam cum inventore ipsum perferendis possimus reiciendis repellendus temporibus.</p>
+
+</body>
+</html>
--- contact.php
+++ contact.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: oscar
- * Date: 22/09/16
- * Time: 21:18
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Contact</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, aut dicta doloremque explicabo rerum sit? Facere neque optio quaerat qui, repellendus reprehenderit repudiandae saepe unde vel. Eius odio saepe totam.</p>
+</body>
+</html>
